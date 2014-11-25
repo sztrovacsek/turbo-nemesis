520c4fd6638b8cbb2cf7cb8af967669276d11f08
Sztrovacsek <hanna.kollo@gmail.com>
2014-11-25 14:54:10 +0100

    login flow backend

--- api/views.py
+++ api/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+import logging
+import json
+import time
 
-# Create your views here.
+import time, os, base64, hmac, urllib
+from hashlib import sha1
+
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse
+
+from django.contrib import messages
+from django import forms
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
+from .models import *
+
+
+logger = logging.getLogger(__name__)
+
+
+def api_backend_login(request):
+    if not request.is_ajax():
+        return HttpResponse()
+
+    if request.user.is_authenticated():
+        return HttpResponse()
+
+    data = request.POST
+    fbUid = data["fbUid"]
+    if fbUid:
+        user = User.objects.filter(username=fbUid)
+        if user:
+            print("Already logged in, wtf? ({0})".format(fbUid));
+        elif user is not None:
+            print("Create and log in user ({0})".format(fbUid));
+            user = User.objects.create_user(
+                username=fbUid,
+                password='dummy')
+            user.save()
+        # TODO: use our own auth backend
+        user = authenticate(username=fbUid, password='dummy')
+        if user is not None:
+            login(request, user)
+        
+    reply = {"reply": "OK", "user": user.username}
+    return HttpResponse(
+        json.dumps(reply, sort_keys=True, separators=(',',':'), indent=4),
+        content_type='application/json'
+    )
+
+
+@login_required
+def api_test(request):
+    reply = {"reply": "OK",}
+    return HttpResponse(
+        json.dumps(reply, sort_keys=True, separators=(',',':'), indent=4),
+        content_type='application/json'
+    )
--- prandius/urls.py
+++ prandius/urls.py
@@ -3,11 +3,18 @@ from django.contrib import admin
 from django.views.generic.base import RedirectView
 from django.contrib.auth import views as auth_views
 
+from api.views import *
+
+
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^login/$', auth_views.login,
         {'template_name': 'api/login.html'}),
 
+    url(r'^api/backend_login/$', api_backend_login),
+    url(r'^api/test/$', api_test),
+
     url(r'^$', RedirectView.as_view(url='/index.html', permanent=False), name='index-view'),
+
 )

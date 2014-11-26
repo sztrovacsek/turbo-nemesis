8640168e938d2afcfd8625b7d6e64028ff216e93
Sztrovacsek <hanna.kollo@gmail.com>
2014-11-26 19:57:48 +0100

    food photo save

--- client/app/view2/view2.js
+++ client/app/view2/view2.js
@@ -12,12 +12,14 @@ angular.module('prandiusApp.view2', ['ngRoute'])
 .controller('View2Ctrl', ['$scope', '$http',
   function($scope, $http) {
     $scope.savePhoto = function(){
+      var photo_url = $('#avatar_url')[0].value;
+      console.log($photo_url);
       console.log($scope.photo_url);
       // post the data to the server
       $.ajax({
         url: "/api/photo_add/",
         headers: {'X-CSRFToken': $.cookie('csrftoken')},
-        data: $scope.photo_url,
+        data: photo_url,
         type: "POST",
         dataType: "JSON",
         success: function(json){

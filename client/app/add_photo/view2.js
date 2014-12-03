064d7de1ec3c806df696b1ddfb8cea24db6332d2
Sztrovacsek <hanna.kollo@gmail.com>
2014-12-03 18:47:30 +0100

    wip

--- client/app/add_photo/view2.js
+++ client/app/add_photo/view2.js
@@ -1,5 +1,23 @@
 'use strict';
 
+function s3_upload(status_elem, url_elem, preview_elem){
+  var s3upload = new S3Upload({
+      file_dom_selector: 'files',
+      s3_sign_put_url: '/sign_s3/',
+      onProgress: function(percent, message) {
+          status_elem.innerHTML = 'Upload progress: ' + percent + '% ' + message;
+      },
+      onFinishS3Put: function(url) {
+          status_elem.innerHTML = 'Upload completed. Uploaded to: '+ url;
+          url_elem.value = url;
+          preview_elem.innerHTML = '<img src="'+url+'" style="width:300px;" />';
+      },
+      onError: function(status) {
+          status_elem.innerHTML = 'Upload error: ' + status;
+      }
+  });
+}
+
 angular.module('prandiusApp.add_photo', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
@@ -11,7 +29,9 @@ angular.module('prandiusApp.add_photo', ['ngRoute'])
 
 .controller('View2Ctrl', ['$scope', '$http',
   function($scope, $http) {
+    $scope.upload_succeeded = false;
     $scope.post_succeeded = false;
+    $scope.logged_in = false;
     $http.get('/api/user_login_status/').success(function(data) {
       $scope.username_i = data["username"];
       $scope.name = data["name"];
@@ -34,6 +54,13 @@ angular.module('prandiusApp.add_photo', ['ngRoute'])
           $scope.post_succeeded = true;
         }
       });
+    }
+    $scope.uploadPhoto = function(){
+      console.log("Upload initiated");
+      var status_elem = $("#status")[0];
+      var url_elem = $("#photo_url")[0];
+      var preview_elem = $("#preview")[0];
+      s3_upload(status_elem, url_elem, preview_elem);
     }
     
   }

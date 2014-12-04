3ee739e14f8457f94bbc6a10934a41f296cb172b
Sztrovacsek <hanna.kollo@gmail.com>
2014-12-04 20:48:43 +0100

    celery setup 7

--- api/tasks.py
+++ api/tasks.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 @shared_task
 def worker_create_thumbnail(post_id):
     logger.debug('Worker: create thumbnail for {0}'.format(post_id))
-    post = Post.objects.filter(post_id).first()
+    post = Post.objects.filter(pk=post_id).first()
     if post:
         services.create_missing_thumbnail(post)
     logger.debug('Worker: done')

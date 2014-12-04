c8fb3610f97148a1788710981b588869c075c0db
Sztrovacsek <hanna.kollo@gmail.com>
2014-12-04 20:12:33 +0100

    celery setup 2

--- prandius/celery.py
+++ prandius/celery.py
@@ -14,3 +14,7 @@ app = Celery('prandius')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
+

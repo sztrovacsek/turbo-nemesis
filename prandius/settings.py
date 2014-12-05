79bc266550c91f0495d26da392854dc293daefe1
Sztrovacsek <hanna.kollo@gmail.com>
2014-12-05 14:08:41 +0100

    security measures (prep for alpha release)

--- prandius/settings.py
+++ prandius/settings.py
@@ -17,15 +17,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!p^i&77r$2du@z8cm4@3&hkcn80k%rxrn265g3g(6t$k^g-n##'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET',
+    '!p^i&77r$2du@z8cm4@3&hkcn80k%rxrn265g3g(6t$k^g-n##'
+)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '.prandius.net']
 
 
 # Application definition

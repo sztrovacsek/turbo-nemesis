a9f7e25dcf47fa773c6cdd9aaa2d1ff4f4727b66
Sztrovacsek <hanna.kollo@gmail.com>
2014-12-13 16:57:24 +0100

    using angular-google-maps

--- client/app/map_feed/view_map.js
+++ client/app/map_feed/view_map.js
@@ -7,23 +7,27 @@ angular.module('prandiusApp.map_feed', [
 
 .config(['$routeProvider', 'uiGmapGoogleMapApiProvider', function($routeProvider, uiGmapGoogleMapApiProvider){
   $routeProvider.when('/map_feed', {
-    templateUrl: 'map_feed/view.html',
-    controller: 'ViewCtrl'
+    templateUrl: 'map_feed/view_map.html',
+    controller: 'ViewMapCtrl'
   });
   uiGmapGoogleMapApiProvider.configure({
       key: 'AIzaSyAgPiMv21OmrvXbLHz6lnWfJLa_M1zqYnw',
-      v: '3.17'
+      v: '3.18'
   });
 }])
 
-.controller('ViewCtrl', ['$scope', '$http', 'uiGmapGoogleMapApi',
+.controller('ViewMapCtrl', ['$scope', '$http', 'uiGmapGoogleMapApi',
   function($scope, $http, uiGmapGoogleMapApi) {
     $scope.todo_text = "todo";
     console.log("map_feed controller: start");
     uiGmapGoogleMapApi.then(function(maps) {
       console.log("map can now be defined");
       //promise
-      $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+      $scope.map = {
+        center: {latitude: 45, longitude: -73},
+        mapTypeId:google.maps.MapTypeId.ROADMAP,
+        zoom: 8
+      };
       console.log("map defined");
     });
   }

6a7757cc13f04000db6101811ea720aad6136051
Sztrovacsek <hanna.kollo@gmail.com>
2014-12-13 11:33:00 +0100

    reverted angular-ui-map

--- client/app/map_feed/view.js
+++ client/app/map_feed/view.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('prandiusApp.map_feed', ['ngRoute', 'ui.map'])
+angular.module('prandiusApp.map_feed', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/map_feed', {
@@ -12,13 +12,5 @@ angular.module('prandiusApp.map_feed', ['ngRoute', 'ui.map'])
 .controller('ViewCtrl', ['$scope', '$http',
   function($scope, $http) {
     $scope.todo_text = "todo";
-    $scope.mapOptions = {
-      center: new google.maps.LatLng(35.784, -78.670),
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };    
-    
-    // google maps stuff
-    console.log("map_feed controller: start");
   }
 ]);

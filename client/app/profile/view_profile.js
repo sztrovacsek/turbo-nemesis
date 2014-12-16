fcf9a708f8fbaec0888cc0591d02f9123178981b
Sztrovacsek <hanna.kollo@gmail.com>
2014-12-16 20:58:13 +0100

    profile view edit and delete (wip)

--- client/app/profile/view_profile.js
+++ client/app/profile/view_profile.js
@@ -14,10 +14,19 @@ angular.module('prandiusApp.profile_page', ['ngRoute'])
     console.log("profile_page controller: start");
     $http.get('/api/currentuser/latest_posts/').success(function(data) {
       $scope.posts = data["posts"];
+      $scope.count = data["count"];
       $.map($scope.posts, function(value){
           value.create_date = moment(value.create_date).fromNow();
         });
     });
+    $scope.clickSave = function(post_id){
+      console.log("Saving... "+post_id);
+    }
+
+    $scope.clickDelete = function(post_id){
+      console.log("Deleting... "+post_id);
+    }
+
     if (typeof FB === "undefined"){
       console.log("FB undefined (still)");
     }

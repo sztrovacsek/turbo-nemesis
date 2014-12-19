fc78abc9269b09cfb6832f32212f707b0a376eb0
Sztrovacsek <hanna.kollo@gmail.com>
2014-12-19 22:13:10 +0100

    moved fb login code from index to controller

--- client/app/app.js
+++ client/app/app.js
@@ -26,6 +26,52 @@ var prandiusApp = angular.module('prandiusApp', [
       console.log("Check");
     });
 
+    // This is called with the results from from FB.getLoginStatus().
+    function statusChangeCallback(response) {
+      console.log('statusChangeCallback');
+      console.log(response);
+      if (response.status === 'connected') {
+        // Logged into your app and Facebook.
+        fbLoginConfirmed(response);
+      } else if (response.status === 'not_authorized') {
+        // The person is logged into Facebook, but not your app.
+      } else {
+        // The person is not logged into Facebook, so we're not sure if
+        // they are logged into this app or not.
+      }
+    }
+
+    // login flow, final step
+    function fbLoginConfirmed(response) {
+      var uid = response.authResponse.userID;
+      var accessToken = response.authResponse.accessToken;
+      console.log('Welcome!  Fetching your information.... ');
+      FB.api('/me', function(response) {
+        console.log('Successful login for: ' + response.name);
+        loginBackend(uid, accessToken, response);
+      });
+    }
+
+    // login to the prandius backend
+    function loginBackend(uid, accessToken, fbData){
+      var csrf = $.cookie('csrftoken');
+      console.log("Backend login: start");
+      $.ajax({
+        url: "/api/backend_login/",
+        headers: {
+          'X-CSRFToken': $.cookie('csrftoken'),
+        },
+        data: {"fbUid": uid, "fbData": fbData, "name": fbData.name, "accessToken": accessToken, 'X-CSRFToken': $.cookie('csrftoken')},
+        type: "POST",
+        dataType: "JSON",
+        success: function(json){
+          console.log("Backend login: post succeeded");
+          // redirect (todo: move this code to angular scope and skip the redirect)
+          location.href = "index.html"
+        }
+      });
+    }
+
     $scope.login = function() {
       if ($scope.logged_in) { return; }
       console.log("Login starting...");
@@ -46,6 +92,7 @@ var prandiusApp = angular.module('prandiusApp', [
         success: function(json){
           console.log("Logout succeeded");
           $scope.logged_in = false;
+          $scope.$apply();
         }
       });
     }

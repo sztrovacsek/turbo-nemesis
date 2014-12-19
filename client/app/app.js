529af6086f12fd62aa7a3783a5aca708abbf83c0
Sztrovacsek <hanna.kollo@gmail.com>
2014-12-19 15:01:49 +0100

    new login flow (frontend)

--- client/app/app.js
+++ client/app/app.js
@@ -16,21 +16,39 @@ var prandiusApp = angular.module('prandiusApp', [
 
 .controller('IndexCtrl', ['$scope', '$http',
   function($scope, $http) {
+    $scope.logged_in = false;
     $scope.todo_text = "todo";
     $http.get('/api/user_login_status/').success(function(data) {
       $scope.name = data["name"];
       $scope.logged_in = data["logged_in"];
-      if (!$scope.logged_in){
-        console.log("Checking facebook login status");
-        FB.getLoginStatus(function(response) {
-          statusChangeCallback(response);
-        });
-      }
     });
     $http.get('/api/csrf_token/').success(function(data) {
-      console.log("Can login");
+      console.log("Check");
     });
 
+    $scope.login = function() {
+      if ($scope.logged_in) { return; }
+      console.log("Login starting...");
+      FB.login(function(response){
+        statusChangeCallback(response);
+      }, {scope: 'public_profile,email,user_friends'});
+    }
+
+    $scope.logout = function() {
+      if (!$scope.logged_in) { return; }
+      console.log("Logout starting...");
+      $.ajax({
+        url: "/api/backend_logout/",
+        headers: {'X-CSRFToken': $.cookie('csrftoken')},
+        data: {},
+        type: "POST",
+        dataType: "JSON",
+        success: function(json){
+          console.log("Logout succeeded");
+          $scope.logged_in = false;
+        }
+      });
+    }
   }
 ])
 
